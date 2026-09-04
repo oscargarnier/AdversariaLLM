5d625e503f283d5b1a7aeeb8a4a40f3505f523f2
ogarnier <ogarnier@login5.head.cm.adastra.cines.fr>
2026-09-04 10:08:35 +0200

    requirements for adastra

--- scripts/gcg.sh
+++ scripts/gcg.sh
@@ -8,7 +8,7 @@
 #SBATCH --error=slurm_outputs/gcg_single_run.out
 #SBATCH --time=1:00:00
 
-srun --ntasks-per-node=1 --cpus-per-task=8  --threads-per-core=1 --	HYDRA_FULL_ERROR=1 python run_attacks.py \
+srun --ntasks-per-node=1 --cpus-per-task=8  --threads-per-core=1 -- python run_attacks.py \
 	    model=$(CURRENT_TARGET_MODEL) \
 	    dataset=adv_behaviors \
 	    datasets.adv_behaviors.idx=2 \
